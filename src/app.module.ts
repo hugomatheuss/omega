9c0bf4ff2950c8d74f8b7ff4528e4e02bc1e27c7
Hugo Matheus <programacao3@acade.com.br>
2021-08-20 14:28:21 -0300

    app module update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PropostaModule } from './proposta/proposta.module';
-import { UsuarioModule } from './usuario/usuario.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
     imports: [
-        TypeOrmModule.forRoot({
-            // type: 'postgres',
-            // host: 'localhost',
-            // port: 15432,
-            // username: 'postgres',
-            // password: 'admin',
-            // database: 'OmegaDB',
-            // autoLoadEntities: true,
-            // synchronize: true,
-        }),
+        TypeOrmModule.forRoot({}),
         AuthModule,
         PropostaModule
     ],
